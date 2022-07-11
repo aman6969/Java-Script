d33f94e04d7fa8e5808ba0c36df680ac59c744cc
Ak <rajpuraksingh78@gmail.com>
2022-07-11 16:06:32 +0530

    Basic of js

---
+++ index.js
@@ -0,0 +1,67 @@
+// // //Operators in Javascript
+// // //Arithmetic Operation
+// // var x=4;
+// // var y=6;
+// // // console.log("addition",x+y);
+// // // console.log("subtraction",x-y);
+// // // console.log("Multiplication",x*y);
+// // // console.log("Division",x/y);
+// // //Assignment operator
+// // var z=x;
+// // z +=2;
+// // console.log(z);
+// // //Comparison operator
+// // let a=4;
+// // let b=8;
+// // console.log(a==b);
+// // console.log(a>=b);
+// // console.log(a<=b);
+// // console.log(a<b);
+// // //logical Operator
+// // console.log(true && true);
+// // console.log(false && true);
+// // console.log(true && false);        // AND OPERATOR
+// // console.log(false && false);
+
+// // console.log(true || true);
+// // console.log(false || true);
+// // console.log(true || false);      // OR OPERATOR
+// // console.log(false || false);
+
+// // console.log(!true);
+// // console.log(!false);
+// // //Variable
+// // // var x=3;
+// // var str1="ram"
+
+// // //Data Types
+// // //1. Primitive datatype
+// // //2. Refernence data type
+
+// // //1.Primitive data types
+// // //numbers
+// // let num2=5;
+// // console.log(num2)
+// // //string
+// // let str1="ram"
+// // console.log(str1)
+// //Boolean
+// //undefined
+// // 2. reference data types
+// //Arrays
+// let marks = {ram:100, aman:99,harsh :50,gaurav:80};
+// console.log(marks);
+// console.log(marks["ram"]);
+// let arr1 =[2,"Ram",null,45];
+// console.log(arr1);
+// console.log(arr1[2])
+
+
+//Conditionals
+let age= 67;
+if(age>18){
+    console.log("You can drink");
+}else{
+    console.log("you can't");
+}
+
